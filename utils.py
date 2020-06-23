f8fd32dd6a672d6f24d7febc4be9e4e06386c138
mathewhuen <mathewhe@gmail.com>
2020-06-23 19:14:35 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,8 +3,9 @@ import math
 
 
 def sig_fig(num, sd):
-#     num = float(num) if type(num)==str else num
-    num = round(num, -len(str(num))+sd)
+    assert type(sd)==int, "sd must be an int greater than zero."
+    assert sd>0, "sd must be an int greater than zero."
+    num = round(num, len(str(num))+sd)
     val = '%.'+str(sd-1)+'E'
     return(val % Decimal(str(num)))
 
